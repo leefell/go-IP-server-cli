4847c2ba973f65cbdbfd30a7d55d233d52d7439a
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-04-02 21:22:26 -0300

    add: code

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"linha-de-comando/app"
+	"log"
+	"os"
+)
+
+func main() {
+	aplicacao := app.Gerar()
+	erro := aplicacao.Run(os.Args)
+
+	if erro != nil {
+		log.Fatal(erro)
+	}
+}
